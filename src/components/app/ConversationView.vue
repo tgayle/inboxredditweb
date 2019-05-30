0fbd1d2046e061060ef2c44f4b2a0d22f65952ac
tgayle <travisgayledev@gmail.com>
2019-05-30 16:42:19 -0500

    Create components for each section of MessagingApp

---
+++ src/components/app/ConversationView.vue
@@ -0,0 +1,15 @@
+<template>
+  <p>a given conversation</p>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+export default Vue.extend({
+
+});
+</script>
+
+
+<style>
+
+</style>
--- src/pages/MessagingApp.vue
+++ src/pages/MessagingApp.vue
@@ -1,27 +1,36 @@
 <template>
-  <v-container fluid>
+  <v-container fluid fill-height class="pa-0">
     <v-layout row>
       <v-flex xs2>
-        Message Content
+      <sidebar />
       </v-flex>
+      <v-divider vertical/>
       <v-flex xs3>
-        Recent Conversations
+      <recent-conversations />
       </v-flex>
+      <v-divider vertical/>
       <v-flex>
-        Message content
+      <conversation-view />
       </v-flex>
+      <v-divider vertical/>
     </v-layout>
   </v-container>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-export default Vue.extend({
 
+import ConversationView from '../components/app/ConversationView.vue';
+import RecentConversations from '../components/app/RecentConversations.vue';
+import Sidebar from '../components/app/Sidebar.vue';
+export default Vue.extend({
+  components: {
+    ConversationView,
+    RecentConversations,
+    Sidebar,
+  },
 });
 </script>
 
-
-<style>
-
+<style lang="scss" scoped>
 </style>
